755575ac8c1d31d693f0b98883025144ad713eb2
manar.afifi <manar.afifi@etu.estia.fr>
2025-02-07 12:18:03 +0100

    Circuit Breaker

--- account/src/main/java/fr/estia/mbds/account/CustomerClient.java
+++ account/src/main/java/fr/estia/mbds/account/CustomerClient.java
@@ -1,19 +1,36 @@
 package fr.estia.mbds.account;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="customer" , url="http://localhost:9090/")
+@FeignClient(name="CUSTOMER")
 public interface CustomerClient {
 
     @GetMapping("/customer/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     public Customer getCustomerById(@PathVariable Long id);
 
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     public List<Customer> getCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception)
+    {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not available");
+        customer.setLastName("Not available");
+        customer.setEmail("Not available");
+        return customer;
+    }
+    default List<Customer> getAllCustomers(Exception exception)
+    {
+        return List.of();
+    }
 }
